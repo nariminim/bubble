4203aa4a1807130c9956b9c271335b4a9d604138
nariminim <nariminim@gmail.com>
2025-10-30 12:16:48 +0900

    add files

---
+++ Bubble.js
@@ -0,0 +1,18 @@
+class Bubble {
+  constructor(x, y, r) {
+    this.r = r;
+    this.c = color(random(150), random(255), 255);
+    this.body = Bodies.circle(x, y, r, {
+      restitution: 0.7,
+      firctionAir: 0.001,
+    });
+    Composite.add(engine.world, this.body);
+  }
+
+  display() {
+    let pos = this.body.position;
+    stroke(this.c);
+    noFill();
+    circle(pos.x, pos.y, this.r * 2);
+  }
+}
